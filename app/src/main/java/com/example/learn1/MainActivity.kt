71b8adbaa9bee39bb82c822af138d56c7130e8f8
Denzel <chiagozieani145@gmail.com>
2019-11-14 13:43:18 +0100

    This is a classic bad UI example but the app is what it says it is that part is true.

--- app/src/main/java/com/example/learn1/MainActivity.kt
+++ app/src/main/java/com/example/learn1/MainActivity.kt
@@ -1,50 +1,46 @@
 package com.example.learn1
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.*
+import androidx.core.content.getSystemService
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var die: ImageView
-    lateinit var die2: ImageView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val roll: Button = findViewById(R.id.roll)
-        val reset: Button = findViewById(R.id.res)
-        roll.setOnClickListener { roller() }
-        reset.setOnClickListener{change()}
-        die = findViewById(R.id.die)
-        die2 = findViewById(R.id.die2)
-        // rise.setOnClickListener{incr()}
+        findViewById<Button>(R.id.gical).setOnClickListener{doit(it)}
+        findViewById<TextView>(R.id.name_text).setOnClickListener{revdoit(it)}
+
+
+
     }
 
-    private fun roller() {
-        die.setImageResource(rolldice())
-        die2.setImageResource(rolldice())
-    }
-    private fun change(){
-        die.setImageResource(R.drawable.empty_dice)
-        die2.setImageResource(R.drawable.empty_dice)
+    private fun doit(view: View){
+        val nam = findViewById<EditText>(R.id.chgnm)
+        val cge = findViewById<TextView>(R.id.name_text)
+        cge.text = nam.text
+        view.visibility = View.GONE
+        nam.visibility = View.GONE
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken,0)
     }
 
-    private fun rolldice(): Int {
-        val randomInt = Random.nextInt(6) + 1
-        return when (randomInt) {
-            1 -> R.drawable.dice_1
-            2 -> R.drawable.dice_2
-            3 -> R.drawable.dice_3
-            4 -> R.drawable.dice_4
-            5 -> R.drawable.dice_5
-            else -> R.drawable.dice_6
-
-        }
+    private fun revdoit (view: View){
+        val gic = findViewById<Button>(R.id.gical)
+        val nam = findViewById<EditText>(R.id.chgnm)
+        val cge = findViewById<TextView>(R.id.name_text)
+        cge.requestFocus()
+        val hian = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        hian.hideSoftInputFromWindow(view.windowToken, 0)
+        view.visibility = View.GONE
+        gic.visibility = View.VISIBLE
+        nam.visibility = View.VISIBLE
 
 
     }
